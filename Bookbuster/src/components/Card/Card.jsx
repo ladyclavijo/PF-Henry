779b97e3f645f5ce3cfffac5229bd6e6b0936513
Juan Martín Salinas <juanmartinsalinas1@gmail.com>
2023-05-30 16:01:17 -0300

    Card & Cards (provisional)

--- Bookbuster/src/components/Card/Card.jsx
+++ Bookbuster/src/components/Card/Card.jsx
@@ -1,5 +1,50 @@
-import React from "react";
+import React, {Fragment} from "react";
 
-export default function Card() {
-  return <div>Card</div>;
+export default function Card({id,title,author,image,isActive}) { // Props que vienen del back, destructuring provisional
+
+    function handleAddCart() { // Acá iría toda la lógica de añadir el libro a la lista de la compra
+
+    }
+    
+    if(isActive) { // Este "if()" es para saber si el libro está en stock o no 
+        return (
+            <Fragment>
+                <Link to={"/books" + id}>
+                    <div className="bookName">
+                        <h3>{title}</h3>
+                    </div>
+                    <div className="info">
+                        <div className="author">
+                            <p>{author}</p>
+                        </div>
+                        <div className="image">
+                            <img src={image} alt={title}/>
+                        </div>
+                        <div className="toCart">
+                            <button onClick={handleAddCart()}></button>
+                        </div>
+                        <div className="rate">
+                        </div>
+                    </div>
+                </Link>
+            </Fragment>
+            )
+    } else {
+        return (
+            <Fragment>
+                <div className="bookName">
+                    <h3>{title}</h3>
+                </div>
+                <div className="info">
+                    <div className="author">
+                        <p>{author}</p>
+                    </div>
+                    <div className="image">
+                        <img src={image} alt={title}/>
+                    </div>
+                    <p>Sold out</p> {/* Línea provisional */}
+                </div>
+            </Fragment>
+            )
+    }
 }
