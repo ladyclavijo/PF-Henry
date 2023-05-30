6b7fee795ffcab516d0b92ab758121bfe84a7e3d
bautistax <jbautistabauza@gmail.com>
2023-05-30 14:54:33 -0300

    Searchbar modified

--- Bookbuster/src/components/SearchBar/SearchBar.jsx
+++ Bookbuster/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import {useDispatch } from 'react-redux';
+import { getBooksByName } from "../../redux/actions";
+import './SearchBar.css';
 
 export default function SearchBar() {
-  return <div>SearchBar</div>;
-}
+  const [books, setBooks] = useState('');
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleSearch = e => {
+    e.preventDefault();
+    if(!books || books === ''){
+      setError(true);
+      return;
+    }
+    setError(false);
+    dispatch(getBooksByName(books));
+
+  };
+  const onChangeInput = e => {
+    setBooks(e.target.value);
+  };
+
+   return (
+  <div>
+    <form onSubmit={handleSearch}>
+      <input type="text" placeholder="Search..." onChange={onChangeInput} name="name" value={books} className={error ? 'error' : ''} />
+      <button type="submit" >Submit</button>
+    </form>
+  </div>
+  );
+  
+};
--- Bookbuster/src/redux/actions/actionsTypes.jsx
+++ Bookbuster/src/redux/actions/actionsTypes.jsx
@@ -1,1 +1,2 @@
 export const GET_BOOKS = "GET_BOOKS";
+export const GET_BOOKS_BY_NAME = "GET_BOOKS_BY_NAME";
