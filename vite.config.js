c7235de1fc56775541a76c967272c8cd1b33c8ef
Fhekz <fm.aldeco@gmail.com>
2023-05-31 14:43:40 -0300

    2nd fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0",
   },
 });
