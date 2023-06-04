6e3ffac420f4094908d2ba7e77a914c35395ee1a
stivliz <stivliz0909@gmail.com>
2023-06-04 18:44:38 -0500

    adding new info to about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,45 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar.jsx";
+import './about.css';
+import Faq from './Faq';
+
 
 export default function About() {
   return (
-  <div>
-    <p>About</p>
-      <Link to="/home">
-        <button>Home</button>
-      </Link>
-      <Link to="/">
-        <button>Landing</button>
-      </Link>
-
     <div>
-      <h1>Who are we?</h1>
-    </div>
+        <NavBar />
+        <div className="containerOne">
+          <div className="divAboutLeft">
+            <h2 className="titles">Who are we?</h2>
+            <p> We are a website that facilitates the online sale of books. We also allow the exchange of books between users of the site. Our goal is to connect book lovers from all over the world, allowing them to buy, sell and exchange books in a simple and secure way.</p>
+          </div>
+        </div>
+        <div className="containerTwo">
+          <div className="divAboutRight">
+            <h2  className="titles">Mission</h2>
+            <p>Our mission is to provide users with access to a wide selection of books in a variety of genres, from classic novels to the latest releases. We work closely with reliable bookstores and vendors to ensure that the books offered are of high quality and in good condition.</p>
+          </div>
+        </div>
+        <div className="containerThree">
+          <div className="divAboutttLeft">
+            <h2  className="titles">Vision</h2>
+            <p>Our vision is to become the preferred destination for online book lovers by offering a reliable and convenient platform to buy, sell and exchange books. We strive to create a global community where users can share their passion for reading and discover new stories.</p>
+          </div>
+        </div>
 
-    <div>
-      <h3>We are a website that facilitates the sale of books online. We also allow the exchange of books between users of the page</h3>
-    </div>
 
-    <div>
-      <h1>Us</h1>
-    </div>
-      <strong>Backend Developers</strong>
-      <ul>
-        <li>Franco Aldeco</li>
-        <li>Lady</li>
-        <li>Stiven Lizarazo</li>
-        <li>Agus</li>
-      </ul>
-    <strong>Frontend Developers</strong>
-    <ul>
-      <li>Bautista Bauzá</li>
-      <li>Juan</li>
-      <li>Roberto</li>
-    </ul>
 
-  </div>
+      <Faq />
+ 
+
+
+        <div className="global">
+          <h2 className="titles">Developer Team</h2>
+          <div className="containerFlex">
+              <div className="column">
+                <h3 className="titluloFront">Frontend Developers</h3>
+                <ul>
+                  <li>Bautista Bauzá</li>
+                  <li>Juan Salinas</li>
+                  <li>Roberto Arregui</li>
+                  </ul>
+              </div>
+              <div className="column">
+              <h3 className="titluloBack">Backend Developers</h3>
+              <ul>
+                <li>Franco Aldeco</li>
+                <li>Lady Clavijo</li>
+                <li>Steven Lizarazo</li>
+                <li>Agustin Juarez</li>
+              </ul>
+              </div>
+          </div>
+        </div>
+        </div> 
   )
-};
+}
 
 
 
---
+++ src/pages/About/accordingData.js
@@ -0,0 +1,22 @@
+let accordingData = [ 
+    {
+        title: 'What book genres are available?',
+        accordionContent: 'We offer a wide range of book genres to suit different interests and preferences. Our collection includes genres such as fiction, computer, economics, history and many more. You can browse our website or use the search function to explore the available genres and discover your next favorite book.'
+      },
+      {
+        title: 'What are the shipping costs?',
+        accordionContent: 'Shipping charges may vary depending on factors such as destination, package weight and shipping method selected. During the checkout process, you will be advised of the specific shipping charges for your order. We strive to offer competitive shipping rates and work with reliable shipping partners to ensure fast and secure delivery of your books.'
+      },
+      {
+        title: 'Are the books new or used?',
+        accordionContent: 'We offer new and used books in our marketplace. Each book specifies whether it is new or used, along with its condition. We strive to provide accurate descriptions and ratings of used books to help you make an informed decision. You can choose the condition and price that best suits your preferences and budget.'
+      },
+      {
+        title: 'Is my personal information secure?',
+        accordionContent: 'Yes, we take the security of your personal information very seriously. We employ industry-standard security measures to protect your data and ensure a safe browsing and shopping experience. We do not share your information with third parties without your consent. For more information, please see our privacy policy.'
+      }
+]
+
+export default accordingData;
+
+
