e00a1aff520e8a33497260355f77bea6589e20f9
Agustin <Agusjua15@gmail.com>
2023-06-04 22:14:29 -0300

    css de detail

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
-import {getBookDetail} from "../../redux/actions/index.jsx";
+import { useParams } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar.jsx";
+import { getBookDetail } from "../../redux/actions/index.jsx";
 import Loader from "../../components/Loader/Loader.jsx";
 import "./Details.css";
+import { TbTruckDelivery } from "react-icons/tb";
 
 export default function Details() {
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
-
+  const [showDescription, setShowDescription] = useState(true);
   const book = useSelector((state) => state.booksDetail);
   const dispatch = useDispatch();
 
@@ -19,33 +21,61 @@ export default function Details() {
       setLoading(false);
     }, 1000);
   }, [dispatch, id]);
+  const handlerShowDescription = () => {
+    setShowDescription(true);
+  };
+  const handlerHideDescription = () => {
+    setShowDescription(false);
+  };
 
   return (
     <div>
-      {loading ? (
+      <NavBar />
+      {!book.author ? (
         <Loader />
       ) : (
         <>
-        <div>
-          <Link to="/home">
-            <div>
-              <span> Back To Home </span>
-            </div>
-          </Link>
-          <img src={book.cover} alt={`${book.title} book`} />
-          <h1>{book.title}</h1>
           <div>
-            <h2>Author: {book.author}</h2>
-            <h2>Description: {book.description}</h2>
-            <h2>Price: {book.price}</h2>
-            <h2>Publisher: {book.publisher}</h2>
-            <h2>Publisher Date: {book.publisher_date}</h2>
-            <h2>Pages: {book.pages}</h2>
-            <h2>Language: {book.language}</h2>
+            <div>
+              <span> </span>
+            </div>
+            <div className="buttons">
+              <button onClick={handlerShowDescription}>Description</button>
+              <button onClick={handlerHideDescription}>More information</button>
+            </div>
+            <div className="body">
+              {showDescription ? (
+                <div className="conteiner1">
+                  <img src={book.cover} alt={`${book.title} book`} />
+                  <div className="conteinerInfo">
+                    <h1>{book.title}</h1>
+                    <h2 className="author">{book.author}</h2>
+                    <h2 className="description"> {book.description}</h2>
+                  </div>
+                </div>
+              ) : (
+                <div className="conteiner1">
+                  <img src={book.cover} alt={`${book.title} book`} />
+                  <div className="conteinerInfo">
+                    <h2 className="moreInfo">Publisher: {book.publisher}</h2>
+                    <h2 className="moreInfo">
+                      Publisher Date: {book.publisher_date}
+                    </h2>
+                    <h2 className="moreInfo">Pages: {book.pages}</h2>
+                    <h2 className="moreInfo">Language: {book.language}</h2>
+                  </div>
+                </div>
+              )}
+              <div className="price">
+                <h1>$ARS {book.price}</h1>
+                <span>
+                  <TbTruckDelivery className="icon" /> Free delivery worldwide!
+                </span>
+              </div>
+            </div>
           </div>
-        </div>
         </>
-      )};
+      )}
     </div>
   );
 }
