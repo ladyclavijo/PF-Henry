62bbc083601cd1348fc9f1d1750b834e358a28d6
RobertoAR23 <robertoarregui23@gmail.com>
2023-06-07 03:23:41 -0600

    create Payment & Articles

--- src/pages/Buy/Buy.jsx
+++ src/pages/Buy/Buy.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ContainerAddress from './Address'
+import Payment from './Payment'
+import Articles from './Articles'
 
 export default function Buy() {
     return (
         <div>
             <ContainerAddress />
+            <Payment />
+            <Articles />
         </div>
     )
 }
---
+++ src/pages/Buy/Payment.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Payment() {
+    return (
+        <div className='bg-black text-white'>
+            Payment Method
+            <h2 className='text-white'>
+                My cards
+            </h2>
+            <form>
+            </form>
+        </div>
+    )
+}
