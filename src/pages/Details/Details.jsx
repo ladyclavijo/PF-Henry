6bcaf52afc8ce2b2355f8238ced7722cbd72ba0e
RobertoAR23 <robertoarregui23@gmail.com>
2023-06-10 16:58:48 -0600

    Buy it finally works

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-import NavBar from "../../components/NavBar/NavBar.jsx";
-import { getBookDetail } from "../../redux/actions/index.jsx";
-import Loader from "../../components/Loader/Loader.jsx";
-import "./Details.css";
-import { useAuth } from "../../context/authContext"
-import { TbTruckDelivery } from "react-icons/tb";
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar.jsx';
+import { getBookDetail } from '../../redux/actions/index.jsx';
+import Loader from '../../components/Loader/Loader.jsx';
+import './Details.css';
+import { useAuth } from '../../context/authContext';
+import { TbTruckDelivery } from 'react-icons/tb';
 
 export default function Details() {
   const { id } = useParams();
@@ -17,6 +15,8 @@ export default function Details() {
   const [showDescription, setShowDescription] = useState(true);
   const book = useSelector((state) => state.booksDetail);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     setLoading(true);
@@ -25,12 +25,28 @@ export default function Details() {
       setLoading(false);
     }, 1000);
   }, [dispatch, id]);
+
   const handlerShowDescription = () => {
     setShowDescription(true);
   };
+
   const handlerHideDescription = () => {
     setShowDescription(false);
   };
+
+  const handleBuyClick = () => {
+    const bookData = {
+      //INFO QUE ENVIA EL BOTON BUY IT-------------------------------------------------------------
+      id: book.id,
+      title: book.title,
+      cover: book.cover,
+      price: book.price,
+      quantity: quantity,
+    };
+    console.log(bookData)
+    navigate('/buy', { state: bookData });
+  };
+
 
   return (
     <div>
@@ -72,8 +88,7 @@ export default function Details() {
                       <h2 className="moreInfo">Genre: {book.genres[0].name}</h2>
                     ) : (
                       <h2 className="moreInfo">
-                        Genres:{" "}
-                        {`${book.genres[0].name}, ${book.genres[1].name}`}
+                        Genres: {`${book.genres[0].name}, ${book.genres[1].name}`}
                       </h2>
                     )}
                     <h2 className="moreInfo">Stock: {book.stock}</h2>
@@ -84,26 +99,36 @@ export default function Details() {
                 <h1>${book.price}</h1>
                 <div className="delivery-container">
                   <span>
-                    <TbTruckDelivery className="icon" /> Free delivery on orders
-                    over $15!
+                    <TbTruckDelivery className="icon" /> Free delivery on orders over $15!
                   </span>
-                  
                   {user ? (
-                  <>
-                    <button className="Add-to-cart">Add to cart</button>
-                    <Link to={"/buy"}>
-                      <button className="Buy-it">Buy it</button>
-                    </Link> 
-                  </>)
-                  : (
-                  <> 
-                    <Link to={"/login"}>
+                    <>
+                      <div className="quantity-select">
+                        <label htmlFor="quantity">Quantity:</label>
+                        <select
+                          id="quantity"
+                          value={quantity}
+                          onChange={(e) => setQuantity(parseInt(e.target.value))}
+                        >
+                          {Array.from({ length: book.stock }, (_, index) => index + 1).map((num) => (
+                            <option key={num} value={num}>{num}</option>
+                          ))}
+                        </select>
+                      </div>
                       <button className="Add-to-cart">Add to cart</button>
-                    </Link> 
-                    <Link to={"/login"}>
-                      <button className="Buy-it">Buy it</button>
-                    </Link> 
-                  </> 
+                      <button className="Buy-it" onClick={handleBuyClick}>
+                        Buy it
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <Link to="/login">
+                        <button className="Add-to-cart">Add to cart</button>
+                      </Link>
+                      <Link to="/login">
+                        <button className="Buy-it">Buy it</button>
+                      </Link>
+                    </>
                   )}
                 </div>
               </div>
