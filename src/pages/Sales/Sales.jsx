178ba9b8fead45e6f7614f807383f3b1071240ac
RobertoAR23 <robertoarregui23@gmail.com>
2023-06-14 15:31:04 -0600

    Route sales

---
+++ src/pages/Sales/Sales.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+
+export default function Sales() {
+    return (
+        <div>
+            <NavBar />
+            <h1 className='mt-11 text-4xl font-crimson-pro'>My products for sale:</h1>
+        </div>
+    );
+}
