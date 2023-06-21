5e053601d77ceb4dc0422e1dc976ef519cb195fd
RobertoAR23 <robertoarregui23@gmail.com>
2023-06-20 23:40:13 -0600

    second graphic

--- src/components/Graphics/ItemSold.jsx
+++ src/components/Graphics/ItemSold.jsx
@@ -1,60 +1,72 @@
-// import React, { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import {
-//   BarChart,
-//   Bar,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//   Legend,
-// } from "recharts";
-// import { getOrders } from "../../redux/actions/index";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getOrders } from "../../redux/actions/index";
+import "./ItemSold.css";
 
-// export default function Graphic() {
-//   const dispatch = useDispatch();
-//   const dailySales = useSelector((state) => state.dailySales);
+export default function ItemSold() {
+    const dispatch = useDispatch();
+    const dailySales = useSelector((state) => state.dailySales);
 
-//   useEffect(() => {
-//     dispatch(getOrders());
-//   }, [dispatch]);
+    useEffect(() => {
+        dispatch(getOrders());
+    }, [dispatch]);
 
-//   // Agrupar las ventas por día
-//   const groupedSales = dailySales.reduce((result, order) => {
-//     const date = new Date(order.createdAt);
-//     const isoDate = date.toISOString().split("T")[0]; // Convertir a formato ISO 8601
+    const renderBarChart = () => {
+        const groupedSales = dailySales.reduce((result, order) => {
+            const date = new Date(order.createdAt);
+            const isoDate = date.toISOString().split("T")[0]; // Convertir a formato ISO 8601
 
-//     if (result[isoDate]) {
-//       result[isoDate] += order.items.reduce(
-//         (acc, item) => (item.qty ? acc + item.qty : acc),
-//         0
-//       );
-//     } else {
-//       result[isoDate] = order.items.reduce(
-//         (acc, item) => (item.qty ? acc + item.qty : acc),
-//         0
-//       );
-//     }
+            if (result[isoDate]) {
+                result[isoDate] += order.items.reduce(
+                    (acc, item) => (item.qty ? acc + item.qty : acc),
+                    0
+                );
+            } else {
+                result[isoDate] = order.items.reduce(
+                    (acc, item) => (item.qty ? acc + item.qty : acc),
+                    0
+                );
+            }
 
-//     return result;
-//   }, {});
+            return result;
+        }, {});
 
-//   // Convertir los datos agrupados en un array para el gráfico
-//   const salesData = Object.keys(groupedSales).map((day) => ({
-//     day,
-//     itemsSold: groupedSales[day],
-//   }));
+        const salesData = Object.keys(groupedSales).map((day) => ({
+            day,
+            itemsSold: groupedSales[day],
+        }));
 
-//   return (
-//     <div>
-//       <h2>Item Sales</h2>
-//       <BarChart width={600} height={500} data={salesData}>
-//         <XAxis dataKey="day" />
-//         <YAxis />
-//         <Tooltip />
-//         <Legend />
-//         <Bar dataKey="itemsSold" fill="rgba(75, 192, 192, 0.6)" barSize={150} />
-//       </BarChart>
-//     </div>
-//   );
-// }
+        const maxItemsSold = Math.max(...Object.values(groupedSales));
+
+        return salesData.map((data) => {
+            const height = (data.itemsSold / maxItemsSold) * 100;
+            const barColor = getBarColor(data.itemsSold);
+
+            return (
+                <div
+                    className="bar"
+                    style={{ height: `${height}%`, backgroundColor: barColor }}
+                    key={data.day}
+                >
+                    <div className="items-sold">{data.itemsSold}</div>
+                    <div className="day">{data.day}</div>
+                </div>
+            );
+        });
+    };
+
+    const getBarColor = (value) => {
+        if (value >= 1) {
+            return "#074a9cc7";
+        }
+        // Puedes agregar más condiciones y retornar diferentes colores según tus necesidades
+        return "#FFFFFF";
+    };
+
+    return (
+        <div className="graphic-container">
+            <h2>Item Sales</h2>
+            <div className="bar-chart-Item">{renderBarChart()}</div>
+        </div>
+    );
+}
