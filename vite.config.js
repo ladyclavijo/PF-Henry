a49b31903b20cc2cb2f15f41bad76072e2697858
Franco Aldeco <109076174+fmaldeco@users.noreply.github.com>
2023-06-22 16:35:15 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,5 @@ export default {
     port: process.env.PORT || 3000,
     host: "0.0.0.0",
   },
-
-  esbuild: {
-    // Opciones de configuración de Esbuild
-    // Ejemplo:
-    jsxInject: `import React from 'react';`,
-  },
 });
 
